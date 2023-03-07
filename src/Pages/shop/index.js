5e98466524d163edd69764f1ab483441f2261734
ahmad-ahmadnejad <ahmad.ahmadnejad.1999@gmail.com>
2023-03-08 00:24:43 +0330

    Create Shop Page

--- src/Pages/shop/index.js
+++ src/Pages/shop/index.js
@@ -1,5 +1,23 @@
+import { Col, Row } from "antd";
+import CustomCard from "../../Components/Card/card";
+import Products from "../../Data/Products/products";
+
 const Shop = () => {
-  return <>فروشگاه</>;
+  return (
+    <Row gutter={16}>
+      {Products.map((item) => (
+        <Col span={8}>
+          <CustomCard
+            key={item?.id}
+            avatar={item?.avatar}
+            title={item?.title}
+            description={item?.description}
+            cover={item?.cover}
+          />
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default Shop;
