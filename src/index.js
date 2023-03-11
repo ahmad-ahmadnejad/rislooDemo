38cacb6c41bca6cce6c2b524ca221a26dfa5c523
ahmad-ahmadnejad <ahmad.ahmadnejad.1999@gmail.com>
2023-03-11 19:22:03 +0330

    Create CartContext API

--- src/index.js
+++ src/index.js
@@ -3,23 +3,27 @@ import ReactDOM from "react-dom/client";
 import { ConfigProvider, theme } from "antd";
 import "./index.css";
 import App from "./App";
-import "./tailwind.css";
 import "./Style/font.css";
 import "./Style/ovverrides.css";
+import "./tailwind.css";
+
 import { BrowserRouter } from "react-router-dom";
 import MainLayout from "./Layout/MainLayout";
+import { CartContextProvider } from "./ContextApi/cartContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <ConfigProvider
-      direction="rtl"
-      theme={{
-        algorithm: theme.defaultAlgorithm,
-      }}
-    >
-      <MainLayout>
-        <App />
-      </MainLayout>
-    </ConfigProvider>
-  </BrowserRouter>
+  <CartContextProvider>
+    <BrowserRouter>
+      <ConfigProvider
+        direction="rtl"
+        theme={{
+          algorithm: theme.defaultAlgorithm,
+        }}
+      >
+        <MainLayout>
+          <App />
+        </MainLayout>
+      </ConfigProvider>
+    </BrowserRouter>
+  </CartContextProvider>
 );
